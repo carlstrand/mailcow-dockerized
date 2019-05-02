aa0267923c8abd3d98cd0a243457c6d2404392b7
carlstrand <43237772+carlstrand@users.noreply.github.com>
2019-05-02 13:08:09 +0200

    Update install-roundcube.sh

--- install-roundcube.sh
+++ install-roundcube.sh
@@ -1,27 +1,33 @@
-git clone https://github.com/roundcube/roundcubemail
+#!/bin/bash
 
-cd roundcubemail
+apt install git
+
+git clone https://github.com/roundcube/roundcubemail && cd roundcubemail
+curl -sS https://getcomposer.org/installer | php -- --install-dir=/tmp/
 
 bin/install-jsdeps.sh
 bin/jsshrink.sh
-bin/updatecss.sh
-bin/cssshrink.sh
 
 cd skins/elastic
+
 lessc -x styles/styles.less > styles/styles.css
 lessc -x styles/print.less > styles/print.css
 lessc -x styles/embed.less > styles/embed.css
+
 cd ../..
 
+bin/updatecss.sh
+bin/cssshrink.sh
+
+rm transifexpull.sh package2composer.sh importgettext.sh exportgettext.sh README.md INSTALL UPGRADING, LICENSE, CHANGELOG
+rm -rf tests/ public_html/ installer/ .git* .tx*
+
+cp composer.json-dist composer.json
+
+php /tmp/composer.phar install --prefer-dist --no-dev
+
+cd ../
+
 mv roundcubemail data/web/rc
+chown -R root: data/web/rc/
 
-
-
-
-
-cd data/web
-wget -O - https://github.com/roundcube/roundcubemail/releases/download/1.3.x/roundcubemail-1.3.x-complete.tar.gz | tar xfvz -
-# Change folder name
-mv roundcubemail-1.3.x rc
-# Change permissions
-chown -R root: rc/
