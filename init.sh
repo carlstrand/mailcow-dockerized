83e2163b58758085c44301a8e4d79762a9c2c174
carlstrand <43237772+carlstrand@users.noreply.github.com>
2019-05-02 13:54:34 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,8 +1,8 @@
-apt-get install software-properties-common
+apt install software-properties-common
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable edge test"
-apt-get update
+apt update
 apt-cache policy docker-ce
-apt-get install -y docker-ce
+apt install -y docker-ce git curl 
 
 curl -L https://github.com/docker/compose/releases/download/1.24.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/
 chmod +x /usr/local/bin/docker-compose
